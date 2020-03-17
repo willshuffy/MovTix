cd3173932c607619050d1bbc44d742e8358691f2
willshuffy <saputrawilly26@gmail.com>
2020-03-18 00:31:01 +0700

    Build Sign Up PhotoScreen

---
+++ app/src/main/java/com/willproject/movtix/SignUpPhotoScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.willproject.movtix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpPhotoScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_photo_screen)
+    }
+}
