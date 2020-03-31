706aff7565cd10ff911813724a7bb93840cc6069
willshuffy <saputrawilly26@gmail.com>
2020-03-31 12:34:53 +0700

    Ticket Show Detail Screen

--- app/src/main/java/com/willproject/movtix/home/TiketFragment.kt
+++ app/src/main/java/com/willproject/movtix/home/TiketFragment.kt
@@ -1,17 +1,30 @@
 package com.willproject.movtix.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.*
 
 import com.willproject.movtix.R
+import com.willproject.movtix.home.dashboard.ComingSoonAdapter
+import com.willproject.movtix.home.model.Film
+import com.willproject.movtix.utils.Preferences
+import kotlinx.android.synthetic.main.fragment_tiket.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class TiketFragment : Fragment() {
+
+    private lateinit var preferences: Preferences
+    lateinit var mDatabase: DatabaseReference
+
+    private var dataList = ArrayList<Film>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +34,40 @@ class TiketFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_tiket, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        preferences = Preferences(activity!!.applicationContext)
+        mDatabase = FirebaseDatabase.getInstance().getReference("Film")
+
+        rc_tiket.layoutManager = LinearLayoutManager(context!!.applicationContext)
+        getData()
+    }
+
+    private fun getData() {
+        mDatabase.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                dataList.clear()
+                for (getdataSnapshoot in dataSnapshot.getChildren()){
+
+                    val film = getdataSnapshoot.getValue(Film::class.java)
+                    dataList.add(film!!)
+                }
+
+                rc_tiket.adapter = ComingSoonAdapter(dataList){
+                    val intent = Intent(context, TiketActivity::class.java).putExtra("data", it)
+                    startActivity(intent)
+                }
+
+                tv_total.setText(dataList.size.toString()+ " movies")
+            }
+
+            override fun onCancelled(error : DatabaseError) {
+                Toast.makeText(context, ""+error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
 }
