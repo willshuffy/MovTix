9f25e165ebb044595f469da0b6571a3916c1b37d
willshuffy <saputrawilly26@gmail.com>
2020-04-01 13:00:50 +0700

    Ticket Show Popup (Custome Dialog)

--- app/src/main/java/com/willproject/movtix/home/TiketActivity.kt
+++ app/src/main/java/com/willproject/movtix/home/TiketActivity.kt
@@ -1,7 +1,17 @@
 package com.willproject.movtix.home
 
+import android.app.Activity
+import android.app.Dialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.willproject.movtix.R
@@ -9,10 +19,12 @@ import com.willproject.movtix.checkout.model.Checkout
 import com.willproject.movtix.home.model.Film
 import kotlinx.android.synthetic.main.activity_tiket.*
 import kotlinx.android.synthetic.main.activity_tiket.iv_back
+import kotlinx.android.synthetic.main.ticket_dialog.view.*
 
 class TiketActivity : AppCompatActivity() {
 
     private var dataList = ArrayList<Checkout>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +44,31 @@ class TiketActivity : AppCompatActivity() {
         dataList.add(Checkout("C1", ""))
         dataList.add(Checkout("C2", ""))
 
-        rc_checkout.adapter = TiketAdapter(dataList){
+        rc_checkout.adapter = TiketAdapter(dataList) {
         }
 
 
         iv_back.setOnClickListener {
             finish()
         }
+
+
+        iv_barcode.setOnClickListener {
+
+            val view = LayoutInflater.from(this).inflate(R.layout.ticket_dialog, null)
+            val alert = AlertDialog.Builder(this, R.style.CustomAlertDialog)
+                .setView(view)
+                .setCancelable(false)
+
+            val mAlertDialog = alert.show()
+            mAlertDialog?.window?.setLayout(500, 700)
+
+            view.btn_close_dialog.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+        }
+
     }
+
+//
 }
