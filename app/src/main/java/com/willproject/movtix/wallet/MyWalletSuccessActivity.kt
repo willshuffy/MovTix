55ba6e9a64224e17a7628af905b8b23672dde93a
willshuffy <saputrawilly26@gmail.com>
2020-04-06 17:10:36 +0700

    Topup Success

---
+++ app/src/main/java/com/willproject/movtix/wallet/MyWalletSuccessActivity.kt
@@ -0,0 +1,12 @@
+package com.willproject.movtix.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyWalletSuccessActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_wallet_success)
+    }
+}
